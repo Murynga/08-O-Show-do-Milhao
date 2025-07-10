a2814e614d3c211406388dd450e85eb6d0fd5821
Walisson <132668335+Murynga@users.noreply.github.com>
2025-07-10 13:01:26 +0000

    criando menu inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Eae men");
+
+/* Funções
+
+menuPrincipal() = para mostrar o menu principal;
+
+
+*/
+
+function menuPrincipal() {
+
+    console.log(`-------<| Show do Milhão |>-------
+                 
+                Bem-vindo! Você deseja...
+                
+                [1] Jogar;
+                [2] Ver placar de líderes;
+                [Outro caractere] para encerrar.
+                
+                Opção: `);
+    
+}
